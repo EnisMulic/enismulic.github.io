e133b24ef750e20f337e75b642c7451ecc990c08
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-07-11 21:27:00 +0200

    Add cv button

---
+++ scripts/cv.js
@@ -0,0 +1,5 @@
+window.onload = function (){
+    var cvLink = document.getElementById("cv").addEventListener("click", function(){
+        window.open('https://github.com/EnisMulic/CurriculumVitae/blob/master/cv.pdf');
+    });
+}
