e672ba6dbde613ea85dfef6ec7357e8b4429456f
Enis Mulić <enis.s.mulic@gmail.com>
2020-12-13 23:27:20 +0100

    Fix scroll button

--- scripts/scroll-button.js
+++ scripts/scroll-button.js
@@ -1,12 +1,12 @@
-window.onload = function (){
-    var scrollButton = document.getElementById("scroll-button");
+document.addEventListener("DOMContentLoaded", function (event) {
+  var scrollButton = document.getElementById("scroll-button");
 
-    if(scrollButton){
-        scrollButton.addEventListener("click", () => window.scrollTo({
-                top: 0, 
-                behavior: 'smooth'
-        
-            })
-        );
-    }
-}
+  if (scrollButton) {
+    scrollButton.addEventListener("click", () =>
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      })
+    );
+  }
+});
