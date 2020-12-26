8d34e688e85b256d77f0a7eecfc265443a78f5e0
Enis Mulić <enis.s.mulic@gmail.com>
2020-12-26 19:34:07 +0100

    Init react

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Portfolio
-
-![](https://img.shields.io/badge/In%20Progress-65%25-red)
+# enismulic.github.io
--- scripts/cv.js
+++
@@ -1,7 +0,0 @@
-window.onload = function (){
-    var cvLink = document.getElementById("cv").addEventListener("click", function(){
-        window.open('CurriculumVitae/cv.pdf', '_blank', 'fullscreen=yes');
-
-        return false;
-    });
-}
