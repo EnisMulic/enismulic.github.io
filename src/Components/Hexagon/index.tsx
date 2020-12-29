d565b6f6cf3d3c3156a1aeb719476dfa263618f2
Enis Mulić <enis.s.mulic@gmail.com>
2020-12-29 20:57:10 +0100

    Add placeholder svgs

--- src/Components/Hexagon/index.tsx
+++ src/Components/Hexagon/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import style from "./Hexagon.module.css";
 
 const Hexagon = (props) => {
-  return <div className={style.hexagon}></div>;
+  return <div className={style.hexagon}>{props.children}</div>;
 };
 
 export default Hexagon;
