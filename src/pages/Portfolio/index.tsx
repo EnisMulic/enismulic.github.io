40645260d16dfd2e685609f4e9c501c8379e7c69
Enis Mulić <enis.s.mulic@gmail.com>
2021-01-13 10:12:47 +0100

    Remove useHistory

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { useHistory } from 'react-router';
 
 import Projects from '../../components/Projects';
 
 import data from '../../placeholderData';
 
 const Portfolio = () => {
-  let history = useHistory();
-
-  history.push('/portfolio');
-
   return (
     <div>
       <div>
