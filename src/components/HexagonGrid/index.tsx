1573038a2d89de10734394453c20ad7b78062934
Enis Mulić <enis.s.mulic@gmail.com>
2021-04-10 21:08:25 +0200

    Add new icons and cleanup code

--- src/components/HexagonGrid/index.tsx
+++ src/components/HexagonGrid/index.tsx
@@ -5,9 +5,40 @@ import style from './HexagonGrid.module.css';
 import Hexagon from '../Hexagon';
 
 const HexagonGrid = () => {
+  const iconClasses = [
+    'devicon-c-line',
+    'devicon-cplusplus-line',
+    'devicon-csharp-line',
+    'devicon-html5-plain',
+    'devicon-css3-plain',
+    'devicon-dot-net-plain-wordmark',
+    'devicon-dotnetcore-plain',
+    'devicon-javascript-plain',
+    'devicon-typescript-plain',
+    'devicon-python-plain',
+    'devicon-git-plain',
+    'devicon-postgresql-plain',
+    'devicon-docker-plain',
+    'devicon-bootstrap-plain',
+    'devicon-bash-plain',
+    'devicon-go-plain',
+    'devicon-react-original',
+    'devicon-mongodb-plain-wordmark',
+    'devicon-jquery-plain-wordmark',
+    'devicon-sqlalchemy-plain',
+    'devicon-microsoftsqlserver-plain-wordmark',
+    'devicon-graphql-plain',
+  ];
   return (
     <div className={style.HexagonGrid}>
-      <Hexagon>
+      {iconClasses.map((iconClass) => {
+        return (
+          <Hexagon>
+            <i className={[style.Icon, iconClass].join(' ')}></i>
+          </Hexagon>
+        );
+      })}
+      {/* <Hexagon>
         <i className={[style.Icon, 'devicon-c-line'].join(' ')}></i>
       </Hexagon>
       <Hexagon>
@@ -81,7 +112,7 @@ const HexagonGrid = () => {
             'devicon-microsoftsqlserver-plain-wordmark',
           ].join(' ')}
         ></i>
-      </Hexagon>
+      </Hexagon> */}
     </div>
   );
 };
