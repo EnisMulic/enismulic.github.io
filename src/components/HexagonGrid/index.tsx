448ac875cc9e7a8e5f9ab4c498cba9598c16dfba
Enis Mulić <enis.s.mulic@gmail.com>
2021-10-25 19:20:55 +0200

    Use devicon-react package

--- src/components/HexagonGrid/index.tsx
+++ src/components/HexagonGrid/index.tsx
@@ -1,42 +1,63 @@
-import React from 'react';
+import CLineIcon from 'react-devicons/c/line';
+import CplusplusLineIcon from 'react-devicons/cplusplus/line';
+import BashPlainIcon from 'react-devicons/bash/plain';
+import BootstrapPlainIcon from 'react-devicons/bootstrap/plain';
+import CsharpLineIcon from 'react-devicons/csharp/plain';
+import Css3PlainIcon from 'react-devicons/css3/plain';
+import DockerPlainIcon from 'react-devicons/docker/plain';
+import DotnetcorePlainIcon from 'react-devicons/dotnetcore/plain';
+import DotNetPlainIcon from 'react-devicons/dot-net/plain';
+import GitPlainIcon from 'react-devicons/git/plain';
+import GoPlainIcon from 'react-devicons/go/plain';
+import GraphqlPlainIcon from 'react-devicons/graphql/plain';
+import Html5PlainIcon from 'react-devicons/html5/plain';
+import JavascriptPlainIcon from 'react-devicons/javascript/plain';
+import JqueryPlainWordmarkIcon from 'react-devicons/jquery/plain';
+import MicrosoftsqlserverPlainWordmarkIcon from 'react-devicons/microsoftsqlserver/plain-wordmark';
+import MongodbPlainWordmarkIcon from 'react-devicons/mongodb/plain-wordmark';
+import PostgresqlPlainIcon from 'react-devicons/postgresql/plain';
+import PythonPlainIcon from 'react-devicons/python/plain';
+import ReactOriginalIcon from 'react-devicons/react/original';
+import SqlalchemyPlainIcon from 'react-devicons/sqlalchemy/plain';
+import TypescriptPlainIcon from 'react-devicons/typescript/plain';
 
 import style from './HexagonGrid.module.css';
 
 import Hexagon from '../Hexagon';
 
 const HexagonGrid = () => {
-  const iconClasses = [
-    'devicon-c-line',
-    'devicon-cplusplus-line',
-    'devicon-csharp-line',
-    'devicon-html5-plain',
-    'devicon-css3-plain',
-    'devicon-dot-net-plain-wordmark',
-    'devicon-dotnetcore-plain',
-    'devicon-javascript-plain',
-    'devicon-typescript-plain',
-    'devicon-python-plain',
-    'devicon-git-plain',
-    'devicon-postgresql-plain',
-    'devicon-docker-plain',
-    'devicon-bootstrap-plain',
-    'devicon-bash-plain',
-    'devicon-go-plain',
-    'devicon-react-original',
-    'devicon-mongodb-plain-wordmark',
-    'devicon-jquery-plain-wordmark',
-    'devicon-sqlalchemy-plain',
-    'devicon-microsoftsqlserver-plain-wordmark',
-    'devicon-graphql-plain',
+  const icons = [
+    <CLineIcon className={style.Icon}></CLineIcon>,
+    <CplusplusLineIcon className={style.Icon}></CplusplusLineIcon>,
+    <CsharpLineIcon className={style.Icon}></CsharpLineIcon>,
+    <Html5PlainIcon className={style.Icon}></Html5PlainIcon>,
+    <Css3PlainIcon className={style.Icon}></Css3PlainIcon>,
+    <DotNetPlainIcon className={style.Icon}></DotNetPlainIcon>,
+    <DotnetcorePlainIcon className={style.Icon}></DotnetcorePlainIcon>,
+    <JavascriptPlainIcon className={style.Icon}></JavascriptPlainIcon>,
+    <TypescriptPlainIcon className={style.Icon}></TypescriptPlainIcon>,
+    <PythonPlainIcon className={style.Icon}></PythonPlainIcon>,
+    <PostgresqlPlainIcon className={style.Icon}></PostgresqlPlainIcon>,
+    <GitPlainIcon className={style.Icon}></GitPlainIcon>,
+    <DockerPlainIcon className={style.Icon}></DockerPlainIcon>,
+    <BootstrapPlainIcon className={style.Icon}></BootstrapPlainIcon>,
+    <BashPlainIcon className={style.Icon}></BashPlainIcon>,
+    <ReactOriginalIcon className={style.Icon}></ReactOriginalIcon>,
+    <MongodbPlainWordmarkIcon
+      className={style.Icon}
+    ></MongodbPlainWordmarkIcon>,
+    <GoPlainIcon className={style.Icon}></GoPlainIcon>,
+    <JqueryPlainWordmarkIcon className={style.Icon}></JqueryPlainWordmarkIcon>,
+    <SqlalchemyPlainIcon className={style.Icon}></SqlalchemyPlainIcon>,
+    <MicrosoftsqlserverPlainWordmarkIcon
+      className={style.Icon}
+    ></MicrosoftsqlserverPlainWordmarkIcon>,
+    <GraphqlPlainIcon className={style.Icon}></GraphqlPlainIcon>,
   ];
   return (
     <div className={style.HexagonGrid}>
-      {iconClasses.map((iconClass) => {
-        return (
-          <Hexagon>
-            <i className={[style.Icon, iconClass].join(' ')}></i>
-          </Hexagon>
-        );
+      {icons.map((icon, index) => {
+        return <Hexagon children={icon} key={index}></Hexagon>;
       })}
     </div>
   );
