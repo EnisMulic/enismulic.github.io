5c805f172c2c07eb9fc16c77452cf85f1b7100cf
Enis Mulic <enis.s.mulic@gmail.com>
2026-06-06 18:57:10 +0200

    chore: remove terminal easter egg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
-import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://enismulic.github.io',
-  integrations: [react(), sitemap()],
+  integrations: [sitemap()],
   output: 'static',
   image: {
     service: passthroughImageService(),
