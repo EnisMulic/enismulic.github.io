c7b86405516454508c50bf1d25b93902d1abe2ed
Enis Mulic <enis.s.mulic@gmail.com>
2026-09-04 23:08:49 +0200

    feat: add setup for blogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
   site: 'https://enismulic.github.io',
@@ -7,5 +8,10 @@ export default defineConfig({
   output: 'static',
   image: {
     service: passthroughImageService(),
+  },
+  markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
+    ],
   },
 });
